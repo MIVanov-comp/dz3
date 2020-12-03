ee80b0e9632781bb6c37137b00f661e3149f5aa4
MIVanov-comp <ugleplastik147@gmail.com>
2020-12-03 17:17:43 +0500

    DZ3

--- dz3/ViewController.swift
+++ dz3/ViewController.swift
@@ -7,13 +7,75 @@
 
 import UIKit
 
+@IBDesignable class PrimaryButton: UIButton{
+    @IBAction func butt(_ sender: Any) {
+    }
+    @IBInspectable var cornerRadius: CGFloat{
+        get{
+            return layer.cornerRadius
+        }
+        set{
+            layer.cornerRadius = newValue
+        }
+    }
+
+    
+    
+    
+}
+
 class ViewController: UIViewController {
 
+    @IBOutlet weak var rectView: UIView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+  
 
+    
 
+    @IBOutlet weak var rectTop: NSLayoutConstraint!
+    
+
+    
+    
+    
+    var isActive: Bool = true
+    var isDown: Bool = true
+    
+    @IBAction func changeColor(_ sender: Any) {
+        isActive = !isActive
+        if isActive{
+            rectView.backgroundColor = .yellow
+        }
+        else{
+            rectView.backgroundColor = .blue
+        }
+    }
+    
+    @IBAction func changeHeight(_ sender: Any) {
+        isDown = !isDown
+        if isDown
+        {
+            rectTop.constant = 400
+            UIView.animate(withDuration: 3){
+                self.view.layoutIfNeeded()
+            }
+        }
+        else{
+            rectTop.constant = 150
+            UIView.animate(withDuration: 3){
+                self.view.layoutIfNeeded()
+            }
+        }
+       
+        
+        
+        
+    }
+    
+    
+    
 }
 
